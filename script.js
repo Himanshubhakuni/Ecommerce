c5d48d4b142eb731b4364cbd200ba13cbc8da1d4
Himanshubhakuni <himanshubhakuni66@gmail.com>
2024-01-10 18:38:27 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function checkLogin() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (email === "admin@admin.com" && password === "123456") {
+        alert("Login successful");
+    } else {
+        alert("Incorrect email or password");
+    }
+}
